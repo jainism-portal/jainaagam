271ce4e55da4a1db0db8abdc1a5cc1ebdd277bcd
manasmadrecha <madrechamanas@gmail.com>
2021-03-10 22:19:18 +0530

    feat: add layout (default)

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,19 @@
 <template>
   <div>
-    <Nuxt />
+    <!-- <TheHeader></TheHeader> -->
+    <Nuxt
+      class="tw-mx-2 tw-py-4"
+      keep-alive
+      :keep-alive-props="{ max: 10 }"
+    ></Nuxt>
+    <!-- <TheFooter></TheFooter> -->
   </div>
 </template>
 
 <style>
 html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;

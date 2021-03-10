9ecf6fb564d0263a5f3d6b35e843344b9e98ef0c
manasmadrecha <madrechamanas@gmail.com>
2021-03-10 22:12:56 +0530

    feat: add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'nuxt.config.js',
+      'content/**/*.md',
+    ],
+    options: {
+      safelist: ['hover:tw-bg-facebook', 'hover:tw-bg-twitter', 'hover:tw-bg-linkedin', 'hover:tw-bg-pinterest']
+    }
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'facebook': '#3b5999',
+        'twitter': '#55acee',
+        'linkedin': '#0077B5',
+        'pinterest': '#bd081c',
+        'instagram': '#e4405f',
+        'whatsapp': '#25D366',
+        'youtube': '#cd201f',
+        'telegram': '0088cc'
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  prefix: 'tw-',
+}

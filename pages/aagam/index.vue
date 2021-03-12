86f037305e3e122a64dca66ab0457afeb6f3a0c6
manasmadrecha <madrechamanas@gmail.com>
2021-03-12 17:53:08 +0530

    feat(page): aagam index to fetch titles from meta

---
+++ pages/aagam/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div v-for="aagam in aagams.aagams" :key="aagam.title">
+      <nuxt-link :to="`/aagam/${aagam.title}`">
+        {{ aagam.title }}
+      </nuxt-link>
+    </div>
+    <pre>
+
+    {{ aagams }}
+    </pre>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      aagams: [],
+    };
+  },
+  async fetch() {
+    this.aagams = await this.$content("aagam-meta", "all").fetch();
+  },
+};
+</script>
+
+<style lang="postcss" scoped>
+</style>

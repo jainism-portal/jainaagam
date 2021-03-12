43f975c7c64d7aba58126d698ebe92068838a1c0
manasmadrecha <madrechamanas@gmail.com>
2021-03-13 01:59:30 +0530

    chore: test a global component for use in md file
    - delete it later

---
+++ components/global/check.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    hello
+    <img :src="src" :alt="src" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: { src: String },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="postcss" scoped>
+</style>

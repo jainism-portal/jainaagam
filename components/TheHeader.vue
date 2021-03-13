333c5634f1af7a53d0be756cb1e91dcf1062a90f
manasmadrecha <madrechamanas@gmail.com>
2021-03-14 03:59:59 +0530

    added basic header

---
+++ components/TheHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <header
+    class="tw-sticky tw-z-30 tw-top-0 tw-h-12 tw-flex tw-justify-evenly tw-items-center tw-bg-gradient-to-br tw-from-white tw-to-pink-50"
+  >
+    <h1>
+      <nuxt-link to="/"> Jainism </nuxt-link>
+    </h1>
+    <nav>
+      <ul class="tw-list-none tw-py-0">
+        <li>
+          <nuxt-link to="/aagam"> Aagam </nuxt-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="postcss" scoped>
+</style>

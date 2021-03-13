42b026046c61f7c0da68b3322c07dfb9bf89b36f
manasmadrecha <madrechamanas@gmail.com>
2021-03-14 02:34:13 +0530

    content body; books v-for; pathComputed for link

--- pages/aagam/_aagam/index.vue
+++ pages/aagam/_aagam/index.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
-    {{ aagam }}
-    {{ books }}
+    <NuxtContent :document="content_aagam[0]"></NuxtContent>
+    <h2>Books</h2>
+    <ol>
+      <li v-for="n in content_aagam[0].children.count" :key="n">
+        <nuxt-link :to="`${pathComputed}book-${n}`"> Book {{ n }} </nuxt-link>
+      </li>
+    </ol>
   </div>
 </template>
 
@@ -9,15 +14,33 @@
 export default {
   data() {
     return {
-      aagams: [],
-      aagam: [],
-      books: [],
+      content_aagam: null,
+      // aagams: [],
+      // aagam: [],
+      // books: [],
     };
   },
+  computed: {
+    pathComputed() {
+      let fullPath;
+      return (fullPath = this.$route.fullPath.endsWith("/")
+        ? this.$route.fullPath
+        : `${this.$route.fullPath}/`);
+    },
+  },
   async fetch() {
-    this.aagams = await this.$content("aagam-meta")
-      .where({ type: "all" })
+    this.content_aagam = await this.$content("hi/aagam", { deep: true })
+      .where({
+        $and: [
+          { type: "aagam" },
+          { dir: { $contains: this.$route.params.aagam } },
+        ],
+      })
       .fetch();
+
+    // this.aagams = await this.$content("aagam-meta")
+    //   .where({ type: "all" })
+    //   .fetch();
 
     // this.aagams_ind = this.aagams.filter((aagamall) => {
     //   for (const aagam of aagamall.aagams) {
@@ -25,20 +48,20 @@ export default {
     //   }
     // });
 
-    for (const aagamsall of this.aagams) {
-      this.aagam = aagamsall.aagams.filter((aagam) => {
-        return aagam.title == this.$route.params.aagam;
-      });
-    }
+    // for (const aagamsall of this.aagams) {
+    //   this.aagam = aagamsall.aagams.filter((aagam) => {
+    //     return aagam.title == this.$route.params.aagam;
+    //   });
+    // }
 
-    for (const aag of this.aagam) {
-      // if (aag.books && aag.books.length > 0) {
-      this.books = aag.books.filter((book) => {
-        return `book-${book.book}/` == this.$route.params.pathMatch;
-        // pathMatch = "book-1/"
-      });
-      // }
-    }
+    //   for (const aag of this.aagam) {
+    //     // if (aag.books && aag.books.length > 0) {
+    //     this.books = aag.books.filter((book) => {
+    //       return `book-${book.book}/` == this.$route.params.pathMatch;
+    //       // pathMatch = "book-1/"
+    //     });
+    //     // }
+    //   }
   },
 };
 </script>

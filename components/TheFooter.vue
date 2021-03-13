b3bf0fc2229c7b59a64c473039aca3c9777d43b8
manasmadrecha <madrechamanas@gmail.com>
2021-03-14 04:07:11 +0530

    basic footer added

---
+++ components/TheFooter.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer
+    class="tw-h-12 tw-flex tw-justify-evenly tw-items-center tw-bg-gradient-to-br tw-from-white tw-to-blue-50"
+  >
+    <div class="tw-flex tw-flex-wrap">
+      <p>&copy; Manas Madrecha - 2021</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="postcss" scoped>
+</style>

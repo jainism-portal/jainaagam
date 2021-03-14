2746b41f87583c7c7cd1e95b570daa73ff34e599
manasmadrecha <madrechamanas@gmail.com>
2021-03-14 20:23:54 +0530

    prakritdictionary added

---
+++ pages/prakritdictionary/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Prakrit Dictionary</h1>
+    <nuxt-link to="/prakritdictionary/word">Words</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="postcss" scoped>
+</style>

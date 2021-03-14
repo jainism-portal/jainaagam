faccaa261e611f56bb38e0663d9c1894235ef4e6
manasmadrecha <madrechamanas@gmail.com>
2021-03-14 20:23:54 +0530

    prakritdictionary added

---
+++ pages/prakritdictionary/word/_slug.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>
+      {{ word.slug }}
+    </h1>
+    <h2>Meaning</h2>
+    <div v-for="mean in word.meanings" :key="mean.meaning">
+      {{ mean.meaning }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      word: [],
+    };
+  },
+  async fetch() {
+    this.word = await this.$content("prakrit/words")
+      .where({ slug: this.$route.params.slug })
+      .fetch();
+    this.word = this.word[0];
+  },
+};
+</script>
+
+<style lang="postcss" scoped>
+</style>
---
+++ pages/prakritdictionary/word/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Prakrit words</h1>
+    <h2>List</h2>
+    <div v-for="word in words" :key="word.slug">
+      <nuxt-link :to="`/prakritdictionary/word/${word.slug}`">
+        {{ word.slug }}
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      words: [],
+    };
+  },
+  async fetch() {
+    this.words = await this.$content("prakrit/words").sortBy("slug").fetch();
+  },
+};
+</script>
+
+<style lang="postcss" scoped>
+</style>

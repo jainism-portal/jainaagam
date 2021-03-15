4f4bca6cf801c28780810a576619b06fabe2f5e9
manasmadrecha <madrechamanas@gmail.com>
2021-03-15 18:09:21 +0530

    show meaning on index page itself as of now

--- pages/prakritdictionary/word/index.vue
+++ pages/prakritdictionary/word/index.vue
@@ -2,11 +2,17 @@
   <div>
     <h1>Prakrit words</h1>
     <h2>List</h2>
-    <div v-for="word in words" :key="word.slug">
-      <nuxt-link :to="`/prakritdictionary/word/${word.slug}`">
-        {{ word.slug }}
-      </nuxt-link>
-    </div>
+    <ol>
+      <li v-for="word in words" :key="word.slug">
+        <nuxt-link :to="`/prakritdictionary/word/${word.slug}`">
+          {{ word.slug }}
+        </nuxt-link>
+        -
+        <span v-for="meaning in word.meanings" :key="meaning.meaning">
+          {{ meaning.meaning }}<span v-if="word.meanings.length > 1">, </span>
+        </span>
+      </li>
+    </ol>
   </div>
 </template>
 

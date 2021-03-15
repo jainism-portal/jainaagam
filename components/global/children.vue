b1e2ee9fc25c9c225af100fd1dda4cae38c63bdb
manasmadrecha <madrechamanas@gmail.com>
2021-03-15 17:25:10 +0530

    children component to add in md files

---
+++ components/global/children.vue
@@ -0,0 +1,29 @@
+<template>
+  <ol>
+    <li v-for="n in children.count" :key="n" class="tw-capitalize">
+      <nuxt-link :to="`${fullPath}${children.type}-${n}`">
+        {{ children.type }} {{ n }}
+      </nuxt-link>
+    </li>
+  </ol>
+</template>
+
+<script>
+export default {
+  props: { children: Object },
+  data() {
+    return {
+      fullPath: "",
+    };
+  },
+  async fetch() {
+    console.log(this.$route);
+    this.fullPath = this.$route.fullPath.endsWith("/")
+      ? this.$route.fullPath
+      : `${this.$route.fullPath}/`;
+  },
+};
+</script>
+
+<style lang="postcss" scoped>
+</style>

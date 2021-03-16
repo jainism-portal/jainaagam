946fdc821ff21a8732d55112da720f28efd715b2
manasmadrecha <madrechamanas@gmail.com>
2021-03-16 07:31:52 +0530

    basic trial info-box made for use in md file

---
+++ components/global/info.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="tw-bg-blue-100">
+    <!-- <header><slot name="header" /></header> -->
+    <slot>Default content</slot>
+  </div>
+</template>
+
+<script>
+export default { name: "AppLayout" };
+</script>

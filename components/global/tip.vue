47b0a9550525f139151bc62773fa095b2e13baa5
manasmadrecha <madrechamanas@gmail.com>
2021-03-19 21:56:04 +0530

    default gray color prop

--- components/global/tip.vue
+++ components/global/tip.vue
@@ -2,17 +2,16 @@
   <div
     class="tw-my-3 tw-px-6 tw-pt-1 tw-pb-4"
     style="border-left-width: 6px"
-    :class="
-      color
-        ? [
-            `tw-text-${color}-800`,
-            `tw-bg-${color}-50`,
-            `tw-border-${color}-500`,
-          ]
-        : ['tw-text-gray-800', 'tw-bg-gray-50', 'tw-border-gray-500']
-    "
+    :class="[
+      `tw-text-${color}-800`,
+      `tw-bg-${color}-50`,
+      `tw-border-${color}-500`,
+    ]"
   >
-    <p class="tw-text-lg tw-font-medium" v-html="title ? title : `Tip`"></p>
+    <p
+      class="tw-pt-2 tw-pb-2 tw-text-lg tw-font-medium"
+      v-html="title ? title : `Tip`"
+    ></p>
     <slot></slot>
   </div>
 </template>
@@ -20,7 +19,7 @@
 <script>
 export default {
   name: "tip",
-  props: { color: String, title: String },
+  props: { color: { type: String, default: "gray" }, title: String },
   data() {
     return {};
   },

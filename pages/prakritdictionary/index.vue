0230d8d5df18e2a94ed5bc9265c02f0dc89b8074
manasmadrecha <madrechamanas@gmail.com>
2021-03-20 06:39:15 +0530

    added tailwind prose classes

--- pages/prakritdictionary/index.vue
+++ pages/prakritdictionary/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="tw-prose lg:tw-prose-lg tw-prose-pink tw-max-w-none">
     <h1>Prakrit Dictionary</h1>
     <nuxt-link to="/prakritdictionary/word">Words</nuxt-link>
   </div>

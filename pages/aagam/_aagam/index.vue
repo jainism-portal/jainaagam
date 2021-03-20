c781a07e01b234c24a0015fd068ad6d2c0541eb4
manasmadrecha <madrechamanas@gmail.com>
2021-03-20 06:32:50 +0530

    ❌ comments; ✔ regex query path; routepath; body

--- pages/aagam/_aagam/index.vue
+++ pages/aagam/_aagam/index.vue
@@ -1,35 +1,18 @@
 <template>
-  <div>
-    <NuxtContent :document="content_aagam"></NuxtContent>
-    <!-- {{ content_aagam }} -->
-    <section v-if="content_aagam">
-      <h2></h2>
-      <nuxt-link :to="`${fullPath}toc`">
-        See Table of Contents of this Aagam</nuxt-link
-      >
-      <h2
-        class="tw-capitalize"
-        v-if="content_aagam.children && content_aagam.children.type"
-      >
-        {{ content_aagam.children.type }}s
-      </h2>
-      <ol
-        v-if="
-          content_aagam &&
-          content_aagam.children &&
-          content_aagam.children.count
-        "
-      >
-        <li v-for="n in content_aagam.children.count" :key="n">
-          <nuxt-link :to="`${fullPath}${content_aagam.children.type}-${n}`">
-            {{ content_aagam.children.type }} {{ n }}
-          </nuxt-link>
-        </li>
-      </ol>
-    </section>
-    <section v-else>
+  <div class="tw-prose lg:tw-prose-lg tw-prose-pink tw-max-w-none">
+    <div v-if="content_aagam">
+      <h1>
+        Jain Aagam
+        <span v-if="content_aagam.title">
+          - {{ content_aagam.title }} सूत्र</span
+        >
+      </h1>
+      <NuxtContent :document="content_aagam"></NuxtContent>
+    </div>
+
+    <div v-else>
       <p>This Aagam is yet to be added</p>
-    </section>
+    </div>
   </div>
 </template>
 
@@ -39,62 +22,25 @@ export default {
   data() {
     return {
       content_aagam: null,
-      fullPath: "",
-      // aagams: [],
-      // aagam: [],
-      // books: [],
+      routePath: "",
     };
   },
   async fetch() {
-    this.fullPath = this.$route.fullPath.endsWith("/")
-      ? this.$route.fullPath
-      : `${this.$route.fullPath}/`;
-
-    // let aagam_list = await this.$content("aagam-meta", "aagam-list").fetch();
+    this.routePath = this.$route.path.endsWith("/")
+      ? this.$route.path
+      : `${this.$route.path}/`;
 
     this.content_aagam = await this.$content("hi/aagam", { deep: true })
       .where({
         $and: [
           { type: "aagam" },
-          { dir: { $regex: [this.$route.params.aagam + "$", "i"] } },
+          { path: { $contains: this.$route.params.aagam } },
         ],
       })
+      .sortBy("slug")
       .fetch();
 
-    // this.content_aagam = this.content_aagam.filter((i) => {
-    //   for (const aagam of aagam_list.aagams) {
-    //     if (aagam.position === i.order.aagam.position) {
-    //       return this.$route.params.aagam === aagam.title;
-    //     }
-    //   }
-    // });
-
     this.content_aagam = this.content_aagam[0];
-
-    // this.aagams = await this.$content("aagam-meta")
-    //   .where({ type: "all" })
-    //   .fetch();
-
-    // this.aagams_ind = this.aagams.filter((aagamall) => {
-    //   for (const aagam of aagamall.aagams) {
-    //     return aagam.title == this.$route.params.aagam;
-    //   }
-    // });
-
-    // for (const aagamsall of this.aagams) {
-    //   this.aagam = aagamsall.aagams.filter((aagam) => {
-    //     return aagam.title == this.$route.params.aagam;
-    //   });
-    // }
-
-    //   for (const aag of this.aagam) {
-    //     // if (aag.books && aag.books.length > 0) {
-    //     this.books = aag.books.filter((book) => {
-    //       return `book-${book.book}/` == this.$route.params.pathMatch;
-    //       // pathMatch = "book-1/"
-    //     });
-    //     // }
-    //   }
   },
 };
 </script>

5fffeb46f7bf6b62bf21338d606f28dbfc832119
manasmadrecha <madrechamanas@gmail.com>
2021-03-20 06:20:39 +0530

    removed different v-if, now single + sutra

--- pages/aagam/_aagam/_.vue
+++ pages/aagam/_aagam/_.vue
@@ -1,21 +1,17 @@
 <template>
   <div class="tw-prose lg:tw-prose-lg tw-prose-pink tw-max-w-none">
     <div v-if="$fetchState.pending">Fetching content... Wait a few seconds</div>
-    <div
-      v-else-if="
-        (content_book === null &&
+    <div v-else-if="$fetchState.error">
+      <!-- (content_book === null &&
           content_section === null &&
           content_part === null &&
           content_chapter === null &&
           content_lesson === null &&
-          content_sutra === null) ||
-        $fetchState.error
-      "
-    >
+          content_sutra === null) || -->
       Wrong URL. Check URL again or go Home.
     </div>
     <div v-else class="tw-mx-2 sm:tw-mx-0">
-      <Incomplete
+      <!-- <Incomplete
         v-if="
           !(
             content_book ||
@@ -32,59 +28,15 @@
         :content_lesson="content_lesson"
         :content_sutra="content_sutra"
         :content_sutra_original="content_sutra_original"
-      ></Incomplete>
+      ></Incomplete> -->
 
-      <article v-if="content_book">
-        <h1>
-          Jain Aagam Book {{ content_book.order.book.position }}
-          <span v-if="content_book.title"> - {{ content_book.title }}</span>
-        </h1>
-        <NuxtContent :document="content_book"></NuxtContent>
-      </article>
-
-      <article v-if="content_section">
-        <h1>
-          Jain Aagam Section {{ content_section.order.section.position }}
-          <span v-if="content_section.title">
-            - {{ content_section.title }}</span
-          >
-        </h1>
-        <NuxtContent :document="content_section"></NuxtContent>
-      </article>
-
-      <article v-if="content_part">
-        <h1>
-          Jain Aagam Part {{ content_part.order.part.position }}
-          <span v-if="content_part.title"> - {{ content_part.title }}</span>
-        </h1>
-        <NuxtContent :document="content_part"></NuxtContent>
-      </article>
-
-      <article v-if="content_chapter">
-        <h1>
-          Jain Aagam Chapter {{ content_chapter.order.chapter.position }}
-          <span v-if="content_chapter.title">
-            - {{ content_chapter.title }}</span
-          >
-        </h1>
-        <NuxtContent :document="content_chapter"></NuxtContent>
-      </article>
-
-      <article v-if="content_lesson">
-        <h1>
-          Jain Aagam Lesson {{ content_lesson.order.lesson.position }}
-          <span v-if="content_lesson.title"> - {{ content_lesson.title }}</span>
-        </h1>
-        <NuxtContent :document="content_lesson"></NuxtContent>
-      </article>
-
-      <article>
+      <article class="">
         <div
-          v-if="content_sutra && content_sutra.order"
-          class="tw-text-center tw-my-2 md:tw-my-4"
+          v-if="content_everyAagamFile && content_everyAagamFile.order"
+          class="tw-bg-white tw-sticky tw-top-14 tw-z-20 tw-border-b tw-border-gray-300 tw-text-center tw-pb-1 tw-mt-2 tw-mb-2 md:tw-mt-0 md:tw-mb-4"
         >
           <p
-            v-for="(items, i) in Object.entries(content_sutra.order)"
+            v-for="(items, i) in Object.entries(content_everyAagamFile.order)"
             :key="i"
             class="tw-inline"
           >
@@ -97,23 +49,36 @@
             </span>
           </p>
         </div>
-        <h1 v-if="content_sutra">
+        <h1 v-if="content_everyAagamFile">
+          Jain Aagam {{ content_everyAagamFile.type }}
+          <!-- {{ content_everyAagamFile.order[content_everyAagamFile.type].position }} -->
+          <span v-if="content_everyAagamFile.title">
+            - {{ content_everyAagamFile.title }}</span
+          >
+        </h1>
+        <h1 v-if="!content_everyAagamFile && content_sutra_original">
+          Jain Aagam {{ content_sutra_original.type }}
+          <!-- {{ content_sutra_original.order[content_sutra_original.type].position }} -->
+          <span v-if="content_sutra_original.title">
+            - {{ content_sutra_original.title }}</span
+          >
+        </h1>
+        <div v-if="content_sutra_original">
+          <h2>Sutra</h2>
+          <NuxtContent :document="content_sutra_original.sutra"></NuxtContent>
+        </div>
+        <section v-if="content_everyAagamFile">
+          <NuxtContent :document="content_everyAagamFile"></NuxtContent>
+        </section>
+      </article>
+
+      <!-- <h1 v-if="content_sutra">
           Aagam Sutra {{ content_sutra.order.sutra.position }}
           <span v-if="content_sutra.title"> - {{ content_sutra.title }}</span>
         </h1>
         <h1 v-if="!content_sutra && content_sutra_original">
           Aagam Sutra {{ content_sutra_original.order.sutra.position }}
-        </h1>
-
-        <section v-if="content_sutra_original">
-          <h2>Sutra</h2>
-          <NuxtContent :document="content_sutra_original.sutra"></NuxtContent>
-        </section>
-        <NuxtContent
-          :document="content_sutra"
-          v-if="content_sutra"
-        ></NuxtContent>
-      </article>
+        </h1> -->
     </div>
   </div>
 </template>
@@ -122,12 +87,13 @@
 export default {
   data() {
     return {
-      content_book: null,
-      content_section: null,
-      content_part: null,
-      content_chapter: null,
-      content_lesson: null,
-      content_sutra: null,
+      content_everyAagamFile: null,
+      // content_book: null,
+      // content_section: null,
+      // content_part: null,
+      // content_chapter: null,
+      // content_lesson: null,
+      // content_sutra: null,
       content_sutra_original: null,
       routePath: "",
       pathMatch: "",
@@ -146,143 +112,35 @@ export default {
 
     // let aagam_list = await this.$content("aagam-meta", "aagam-list").fetch();
 
-    // Aagam content book fetch
-    if (new RegExp(/book-[0-9]+\/$/i).test(this.routePath)) {
-      this.content_book = await this.$content("hi/aagam", { deep: true })
-        .where({
-          $and: [
-            { path: { $contains: this.$route.params.aagam } },
-            {
-              path: {
-                $regex: [
-                  this.pathMatch.slice(0, this.pathMatch.length - 1) + "$",
-                  "gim",
-                ],
-              },
+    // Aagam content everything
+    this.content_everyAagamFile = await this.$content("hi/aagam", {
+      deep: true,
+    })
+      .where({
+        $and: [
+          { path: { $contains: this.$route.params.aagam } },
+          {
+            path: {
+              $regex: [
+                this.pathMatch.slice(0, this.pathMatch.length - 1) + "$",
+                "gim",
+              ],
             },
-          ],
-        })
-        .sortBy("slug")
-        .fetch();
+          },
+        ],
+      })
+      .sortBy("slug")
+      .fetch();
 
-      this.content_book = this.content_book[0];
-    }
+    this.content_everyAagamFile = this.content_everyAagamFile[0];
 
-    // Aagam content Section fetch
-    if (new RegExp(/section-[0-9]+\/$/i).test(this.routePath)) {
-      this.content_section = await this.$content("hi/aagam", { deep: true })
-        .where({
-          $and: [
-            { path: { $contains: this.$route.params.aagam } },
-            {
-              path: {
-                $regex: [
-                  this.pathMatch.slice(0, this.pathMatch.length - 1) + "$",
-                  "gim",
-                ],
-              },
-            },
-          ],
-        })
-        .sortBy("slug")
-        .fetch();
-
-      this.content_section = this.content_section[0];
-    }
-
-    // Aagam content Part fetch
-    if (new RegExp(/part-[0-9]+\/$/i).test(this.routePath)) {
-      this.content_part = await this.$content("hi/aagam", { deep: true })
-        .where({
-          $and: [
-            { path: { $contains: this.$route.params.aagam } },
-            {
-              path: {
-                $regex: [
-                  this.pathMatch.slice(0, this.pathMatch.length - 1) + "$",
-                  "gim",
-                ],
-              },
-            },
-          ],
-        })
-        .sortBy("slug")
-        .fetch();
-
-      this.content_part = this.content_part[0];
-    }
-
-    // Aagam content chapter fetch
-
-    if (new RegExp(/chapter-[0-9]+\/$/i).test(this.routePath)) {
-      this.content_chapter = await this.$content("hi/aagam", { deep: true })
-        .where({
-          $and: [
-            { path: { $contains: this.$route.params.aagam } },
-            {
-              path: {
-                $regex: [
-                  this.pathMatch.slice(0, this.pathMatch.length - 1) + "$",
-                  "gim",
-                ],
-              },
-            },
-          ],
-        })
-        .sortBy("slug")
-        .fetch();
-
-      this.content_chapter = this.content_chapter[0];
-    }
-
-    // Aagam content Lesson fetch
-    if (new RegExp(/lesson-[0-9]+\/$/i).test(this.routePath)) {
-      this.content_lesson = await this.$content("hi/aagam", { deep: true })
-        .where({
-          $and: [
-            { path: { $contains: this.$route.params.aagam } },
-            {
-              path: {
-                $regex: [
-                  this.pathMatch.slice(0, this.pathMatch.length - 1) + "$",
-                  "gim",
-                ],
-              },
-            },
-          ],
-        })
-        .sortBy("slug")
-        .fetch();
-
-      this.content_lesson = this.content_lesson[0];
-    }
-
-    // Aagam content Sutra fetch
     if (new RegExp(/sutra-[0-9]+\/$/i).test(this.routePath)) {
-      this.content_sutra = await this.$content("hi/aagam", { deep: true })
-        .where({
-          $and: [
-            { path: { $contains: this.$route.params.aagam } },
-            {
-              path: {
-                $regex: [
-                  this.pathMatch.slice(0, this.pathMatch.length - 1) + "$",
-                  "gim",
-                ],
-              },
-            },
-          ],
-        })
-        .sortBy("slug")
-        .fetch();
-
-      this.content_sutra = this.content_sutra[0];
-
-      this.content_sutra_original = await this.$content("hi/aagam", {
+      this.content_sutra_original = await this.$content("aagam", {
         deep: true,
       })
         .where({
           $and: [
+            { type: "sutra" },
             { path: { $contains: this.$route.params.aagam } },
             {
               path: {

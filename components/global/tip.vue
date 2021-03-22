e4ceb802b9eaa738ebd5a9893a16304aa7c5f0d2
manasmadrecha <madrechamanas@gmail.com>
2021-03-22 23:11:18 +0530

    refactored code

--- components/global/tip.vue
+++ components/global/tip.vue
@@ -2,11 +2,7 @@
   <div
     class="custom-tip tw-my-3 tw-px-6 tw-pt-1 tw-pb-4"
     style="border-left-width: 6px"
-    :class="[
-      `tw-text-${colorComputed}-800`,
-      `tw-bg-${colorComputed}-50`,
-      `tw-border-${colorComputed}-500`,
-    ]"
+    :class="colorComputed"
   >
     <!-- <p class="tw-font-medium tw-my-2" v-html="title ? title : `Tip`"></p> -->
     <slot></slot>
@@ -26,33 +22,33 @@ export default {
   },
   computed: {
     colorComputed() {
-      if (
-        (this.color == null || this.color == undefined) &&
-        (this.colour == null || this.colour == undefined)
-      ) {
-        return `gray`;
+      // if (
+      //   (this.color == null || this.color == undefined) &&
+      //   (this.colour == null || this.colour == undefined)
+      // ) {
+      //   return [`tw-bg-gray-50`, `tw-text-gray-800`, `tw-border-gray-500`];
+      // } else {
+      let c = this.color ? this.color : this.colour ? this.colour : "gray";
+
+      let isColor =
+        c === "gray" ||
+        c === "red" ||
+        c === "yellow" ||
+        c === "green" ||
+        c === "blue" ||
+        c === "indigo" ||
+        c === "purple" ||
+        c === "pink" ||
+        c === "white";
+
+      let errorMsg = `You have typed wrong color for "tip". Choose only gray, red, yellow, green, blue, indigo, purple, pink or white.`;
+
+      if (isColor) {
+        return [`tw-bg-${c}-50`, `tw-text-${c}-800`, `tw-border-${c}-500`];
       } else {
-        let c = this.color ? this.color : this.colour;
-
-        let isColor =
-          c === "gray" ||
-          c === "red" ||
-          c === "yellow" ||
-          c === "green" ||
-          c === "blue" ||
-          c === "indigo" ||
-          c === "purple" ||
-          c === "pink" ||
-          c === "white";
-
-        let errorMsg = `You have typed wrong color for "tip". Choose only gray, red, yellow, green, blue, indigo, purple, pink or white.`;
-
-        if (isColor) {
-          return c;
-        } else {
-          console.error(errorMsg);
-        }
+        console.error(errorMsg);
       }
+      // }
     },
   },
 };

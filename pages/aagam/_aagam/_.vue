ecaeb4a91e15f6347e84edcd117c6fb4abb51571
manasmadrecha <madrechamanas@gmail.com>
2021-03-22 05:56:40 +0530

    message for incomplete post

--- pages/aagam/_aagam/_.vue
+++ pages/aagam/_aagam/_.vue
@@ -6,69 +6,77 @@
       Wrong URL. Check URL again or go Home.
     </div>
     <div v-else class="tw-mx-2 sm:tw-mx-0">
-      <Incomplete
-        v-if="!(content_everyAagamFile || content_sutra_original)"
-        :content_sutra="content_everyAagamFile"
-        :content_sutra_original="content_sutra_original"
-      ></Incomplete>
-
-      <article class="">
-        <header
-          v-if="content_everyAagamFile && content_everyAagamFile.order"
-          class="tw-bg-white tw-sticky tw-top-14 tw-z-20 tw-border-b tw-border-gray-300 tw-text-center tw-pt-1 tw-pb-2 tw-mb-2 md:tw-mt-0 md:tw-mb-4"
-        >
-          <nav class="tw-flex tw-flex-wrap tw-justify-center tw-items-center">
-            <p class="tw-inline tw-mr-2">
-              <nuxt-link
-                :to="`/${$i18n.locale}/aagam/${$route.params.aagam}/table-of-contents`"
-                class="tw-text-pink-600 tw-underline tw-font-medium tw-capitalize"
-                >{{ $route.params.aagam }} Aagam Index</nuxt-link
-              >
-              |
-            </p>
-            <p
-              v-for="(items, i) in Object.entries(content_everyAagamFile.order)"
-              :key="i"
-              class="tw-inline"
-            >
-              <span v-for="(item, i2) in items" :key="i2">
-                <span v-if="typeof item === 'string' && item != 'cat'"
-                  >{{ item }}-</span
-                ><span v-if="typeof item === 'object'"
-                  >{{ item.position }} →
-                </span>
-              </span>
-            </p>
-          </nav>
-        </header>
-        <main class="tw-prose lg:tw-prose-lg tw-prose-pink tw-max-w-none">
-          <h1 v-if="content_everyAagamFile">
-            {{ $t("jain") }} {{ $t("aagam") }} {{ content_everyAagamFile.type }}
-            <span v-if="content_everyAagamFile.title">
-              - {{ content_everyAagamFile.title }}</span
-            >
-          </h1>
-          <h1 v-if="!content_everyAagamFile && content_sutra_original">
-            {{ $t("jain") }} {{ $t("aagam") }} {{ content_sutra_original.type }}
-            <span v-if="content_sutra_original.title">
-              - {{ content_sutra_original.title }}</span
-            >
-          </h1>
-          <a
-            :href="`https://github.com/madrecha/jainaagam/tree/main/content${content_everyAagamFile.path}${content_everyAagamFile.extension}`"
-            target="_blank"
+      <div v-if="content_everyAagamFile || content_sutra_original">
+        <article class="">
+          <header
+            v-if="content_everyAagamFile && content_everyAagamFile.order"
+            class="tw-bg-white tw-sticky tw-top-14 tw-z-20 tw-border-b tw-border-gray-300 tw-text-center tw-pt-1 tw-pb-2 tw-mb-2 md:tw-mt-0 md:tw-mb-4"
           >
-            Edit on Github
-          </a>
-          <div v-if="content_sutra_original">
-            <h2>Sutra</h2>
-            <NuxtContent :document="content_sutra_original.sutra"></NuxtContent>
-          </div>
-          <section v-if="content_everyAagamFile">
-            <NuxtContent :document="content_everyAagamFile"></NuxtContent>
-          </section>
-        </main>
-      </article>
+            <nav class="tw-flex tw-flex-wrap tw-justify-center tw-items-center">
+              <p class="tw-inline tw-mr-2">
+                <nuxt-link
+                  :to="`/${$i18n.locale}/aagam/${$route.params.aagam}/table-of-contents`"
+                  class="tw-text-pink-600 tw-underline tw-font-medium tw-capitalize"
+                  >{{ $route.params.aagam }} Aagam Index</nuxt-link
+                >
+                |
+              </p>
+              <p
+                v-for="(items, i) in Object.entries(
+                  content_everyAagamFile.order
+                )"
+                :key="i"
+                class="tw-inline"
+              >
+                <span v-for="(item, i2) in items" :key="i2">
+                  <span v-if="typeof item === 'string' && item != 'cat'"
+                    >{{ item }}-</span
+                  ><span v-if="typeof item === 'object'"
+                    >{{ item.position }} →
+                  </span>
+                </span>
+              </p>
+            </nav>
+          </header>
+          <main class="tw-prose lg:tw-prose-lg tw-prose-pink tw-max-w-none">
+            <h1 v-if="content_everyAagamFile">
+              {{ $t("jain") }} {{ $t("aagam") }}
+              {{ content_everyAagamFile.type }}
+              <span v-if="content_everyAagamFile.title">
+                - {{ content_everyAagamFile.title }}</span
+              >
+            </h1>
+            <h1 v-if="!content_everyAagamFile && content_sutra_original">
+              {{ $t("jain") }} {{ $t("aagam") }}
+              {{ content_sutra_original.type }}
+              <span v-if="content_sutra_original.title">
+                - {{ content_sutra_original.title }}</span
+              >
+            </h1>
+            <a
+              :href="`https://github.com/madrecha/jainaagam/tree/main/content${content_everyAagamFile.path}${content_everyAagamFile.extension}`"
+              target="_blank"
+            >
+              Edit on Github
+            </a>
+            <div v-if="content_sutra_original">
+              <h2>Sutra</h2>
+              <NuxtContent
+                :document="content_sutra_original.sutra"
+              ></NuxtContent>
+            </div>
+            <section v-if="content_everyAagamFile">
+              <NuxtContent :document="content_everyAagamFile"></NuxtContent>
+            </section>
+          </main>
+        </article>
+      </div>
+      <div v-else>
+        <Incomplete
+          :content_everyAagamFile="content_everyAagamFile"
+          :content_sutra_original="content_sutra_original"
+        ></Incomplete>
+      </div>
     </div>
   </div>
 </template>

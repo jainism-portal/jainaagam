fd69068e68bc93922105b709d49d53c3f9ae7dd0
manasmadrecha <madrechamanas@gmail.com>
2021-04-16 00:06:41 +0530

    moved pages to archive

---
+++ archivecode/inside-aagam-index-15-04-2021.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="tw-prose lg:tw-prose-lg tw-prose-pink tw-max-w-none">
+    <nuxt-link :to="`/${$i18n.locale}/aagam`">Back to all Aagams</nuxt-link>
+    <div v-if="content_aagam">
+      <h1>
+        {{ $t("jain") }} {{ $t("aagam") }}
+        <span v-if="content_aagam.title">
+          - {{ content_aagam.title }} सूत्र</span
+        >
+      </h1>
+      <div class="tw-flex tw-flex-wrap">
+        <a
+          :href="`https://github.com/madrecha/jainaagam/tree/main/content${content_aagam.path}${content_aagam.extension}`"
+          target="_blank"
+          class="tw-mx-2"
+        >
+          Edit on Github
+        </a>
+        <nuxt-link
+          :to="`${$route.params.aagam}/table-of-contents`"
+          class="tw-mx-2"
+          >TOC</nuxt-link
+        >
+      </div>
+      <NuxtContent :document="content_aagam"></NuxtContent>
+    </div>
+
+    <div v-else>
+      <p>This Aagam is yet to be added</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "aagamIndividual", // to avoid console error --> [Vue warn]: Invalid component name
+  data() {
+    return {
+      content_aagam: null,
+      routePath: "",
+    };
+  },
+  async fetch() {
+    this.routePath = this.$route.path.endsWith("/")
+      ? this.$route.path
+      : `${this.$route.path}/`;
+
+    this.content_aagam = await this.$content(`${this.$i18n.locale}/aagam`, {
+      deep: true,
+    })
+      .where({
+        $and: [
+          { type: "aagam" },
+          { path: { $contains: this.$route.params.aagam } },
+        ],
+      })
+      .sortBy("slug")
+      .fetch();
+
+    this.content_aagam = this.content_aagam[0];
+  },
+};
+</script>
+
+<style lang="postcss" scoped>
+</style>

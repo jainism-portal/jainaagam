94c11387f216eb0071b14243f3294743177d736f
manasmadrecha <madrechamanas@gmail.com>
2021-04-16 00:07:37 +0530

    pdf components with test for quick non-global edit

---
+++ components/global/pdf-embed.vue
@@ -0,0 +1,26 @@
+<template>
+  <center class="tw-border-gray-300 tw-border tw-py-1">
+    <iframe
+      :src="pdf"
+      class="tw-w-full tw-h-96"
+      No
+      iframe
+      support
+      by
+      browser.
+    ></iframe>
+    <a :href="pdf" target="_blank">Download PDF</a>
+  </center>
+</template>
+
+<script>
+export default {
+  props: { pdf: { type: String } },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>

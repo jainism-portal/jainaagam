23e23dd2d3fe37ee09f8bc3f981678fa5799d000
manasmadrecha <madrechamanas@gmail.com>
2021-04-16 00:03:34 +0530

    plugins added

---
+++ plugins/vuetify.options.js
@@ -0,0 +1,18 @@
+// vuetify.options.js
+
+// import colors from 'vuetify/es5/util/colors'
+
+export default {
+  // breakpoint: {},
+  // lang: {},
+  // rtl: true,
+  theme: {
+    disable: true
+    // light: {
+    //   anchor: '#fff',
+    // }
+  },
+  icons: {
+    iconfont: 'mdi'
+  }
+}

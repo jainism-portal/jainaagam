1a398deaecbdfb49d9b1ddd87a6d70d3383beb45
manasmadrecha <madrechamanas@gmail.com>
2021-04-16 14:22:37 +0530

    temporary footer added

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,12 +1,26 @@
-
-
 <template>
-  <footer
-    class="tw-flex tw-justify-evenly tw-items-center tw-bg-gradient-to-br tw-from-white tw-to-blue-50"
-  >
-    <div class="tw-flex tw-flex-wrap">
-      <p>&copy; {{ $t("name") }} {{ $t("surname") }} - 2021</p>
-    </div>
+  <footer class="tw-my-2 md:tw-flex">
+    <section class="tw-flex tw-justify-center tw-items-start">
+      <p>&copy; {{ $t("name") }} {{ $t("surname") }} - 2020 - {{new Date().getFullYear()}}</p>
+      <a
+        href="https://instagram.com/ManasMadrecha"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <span class="mdi mdi-instagram tw-text-pink-800 tw-text-xl"></span>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/manasmadrecha/"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="tw-inline-block"
+      >
+        <span class="mdi mdi-linkedin tw-text-blue-800 tw-text-xl"></span>
+      </a>
+    </section>
+    <section class="tw-mx-2">
+      <p>This is a Work-in-Progress project. Your contributions are most welcome.</p>
+    </section>
   </footer>
 </template>
 

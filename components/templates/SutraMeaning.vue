628a78e8328c207f342592e1de6a58c602a5fec3
CA Manas Madrecha <65993481+ManasMadrecha@users.noreply.github.com>
2021-06-12 04:38:20 +0530

    feat(sutra) meaning and explanation slots

---
+++ components/templates/SutraMeaning.vue
@@ -0,0 +1,18 @@
+<template>
+  <section
+    class="nuxt-content"
+    v-if="$scopedSlots.default"
+  >
+    <h2 class="tw-capitalize">{{$t("basic.jain_aagam")}} {{$t("pages.sutra.main.meaning")}}</h2>
+    <div>
+      <slot></slot>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="sass"src="~/assets/css/all.sass" scoped>
+</style>

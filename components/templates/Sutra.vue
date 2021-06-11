b00dff67f20a8af4414c47b9b2345dbfe433ee4d
CA Manas Madrecha <65993481+ManasMadrecha@users.noreply.github.com>
2021-06-12 04:05:42 +0530

    feat(sutra): sutra with og and transliteration
    1. "sanscript" for transliteration
    2. v-tabs for displaying
    3. "flat" for converting sutra body to object
    4. style

---
+++ components/templates/Sutra.vue
@@ -0,0 +1,86 @@
+<template>
+  <v-tabs
+    show-arrows
+    :slider-size="1"
+    v-if="sutraOriginal"
+  >
+    <v-tab
+      data-tab
+      active-class="tw-bg-gradient-to-t tw-from-white tw-via-white tw-to-yellow-50 !tw-text-yellow-900"
+      class="!tw-text-base md:!tw-text-lg !tw-capitalize tw-flex tw-flex-wrap tw-flex-col sm:tw-flex-row sm:tw-gap-2"
+    >
+      <span>{{$t("pages.sutra.main.sutra.tabs.name")}}</span>
+      <span class="tw-text-xs md:tw-text-sm">({{$t("pages.sutra.main.sutra.tabs.original")}})</span>
+    </v-tab>
+    <v-tabs-slider class="tw-bg-gradient-to-br tw-from-white tw-to-yellow-400"></v-tabs-slider>
+    <v-tab-item
+      lang="pka-Deva"
+      class="tw-mt-3 tw-text-gray-600"
+      eager
+    >
+      <nuxt-content
+        :document="sutraOriginal"
+        class="tw-border tw-px-2 md:tw-px-4"
+      ></nuxt-content>
+    </v-tab-item>
+    <v-tab
+      data-tab
+      active-class="tw-bg-gradient-to-t tw-from-white tw-via-white tw-to-yellow-50 !tw-text-yellow-900"
+      class="!tw-text-base md:!tw-text-lg !tw-capitalize tw-flex tw-flex-wrap tw-flex-col sm:tw-flex-row sm:tw-gap-2"
+    >
+      <span>{{$t("pages.sutra.main.sutra.tabs.name")}}</span>
+      <span class="tw-text-xs md:tw-text-sm">({{$t("pages.sutra.main.sutra.tabs.transliteration")}})</span>
+    </v-tab>
+    <v-tab-item
+      lang="pkr-Latn"
+      class="tw-mt-6 tw-text-gray-600"
+      eager
+    >
+      <nuxt-content
+        :document="sutraLatn"
+        class="tw-border tw-px-2 md:tw-px-4"
+      ></nuxt-content>
+    </v-tab-item>
+  </v-tabs>
+</template>
+
+<script>
+const flatten = require("flat");
+const unflatten = require("flat").unflatten;
+
+export default {
+  props: { sutraOriginal: Object },
+  data() {
+    return {
+      sutra: this.sutraOriginal,
+      sutraLatn: null
+    };
+  },
+  async fetch() {
+    const sans = require("@sanskrit-coders/sanscript");
+
+    let a = flatten(this.sutra);
+
+    let b = Object.entries(a).map(child => {
+      if (child[0].endsWith("value")) {
+        child[1] = sans.t(child[1], "devanagari", "iast");
+      } else child[1] = child[1];
+      return [child[0], child[1]];
+    });
+
+    let c = Object.fromEntries(b);
+
+    this.sutraLatn = unflatten(c);
+  }
+};
+</script>
+
+<style lang="sass" scoped>
+.v-slide-group__next, .v-slide-group__prev
+  flex: 0 1 32px !important
+// [data-tab]
+//   @apply tw-text-sm md:tw-text-base
+</style>
+
+<style lang="sass" src="~/assets/css/all.sass" scoped>
+</style>

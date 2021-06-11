3d9da6a1be29520b5fa1cdc76a62a4a55f4bf8f8
CA Manas Madrecha <65993481+ManasMadrecha@users.noreply.github.com>
2021-06-12 04:01:47 +0530

    feat(pages): text and link in incomplete component

--- components/Incomplete.vue
+++ components/Incomplete.vue
@@ -1,36 +1,14 @@
 <template>
-  <div class="tw-mx-4">
-    <!-- v-if="content_everyAagamFile === null || content_sutra_original === null" -->
-    <header class="tw-text-center">
-      <h1 class="tw-text-3xl tw-font-medium tw-text-pink-800">Work in Progress
-      </h1>
-      <p class="tw-text-gray-500">This page is yet to be added.</p>
-    </header>
-    <div class="tw-flex tw-flex-col md:tw-flex-row tw-justify-center tw-items-center tw-gap-4">
-      <div class="list-item">
-        This page is not yet available in {{$i18n.localeProperties.name}} language.
-        <div>
-          Choose different language from Sidebar.
-        </div>
-      </div>
-      OR
-      <div class="list-item">Go <nuxt-link
-          :to="localePath(`/`)"
-          class="!tw-text-blue-700"
-        >Home</nuxt-link> to navigate to the Aagam of your choice.
-      </div>
-    </div>
-    <!-- <div
-      v-if="
-        content_book === null &&
-        content_part === null &&
-        content_chapter === null &&
-        content_lesson === null &&
-        (content_sutra === undefined || content_sutra_original === undefined)
-      "
-    >
-      <h1>This Sutra is yet to be added.</h1>
-    </div> -->
+  <!-- v-if="content_everyAagamFile === null || content_sutra_original === null" -->
+  <div class="tw-mt-3 tw-text-gray-600 tw-text-center">
+    <p class="">This page is yet to be added in {{$i18n.localeProperties.name}} language. </p>
+    <language-input class="tw-mb-3 tw-p-2 tw-inline-block tw-bg-pink-50 tw-border  tw-border-pink-300 tw-transform-gpu tw-transition-transform hover:tw-scale-110"></language-input>
+    <p>Or go Home <nuxt-link
+        :to="localePath(`/`)"
+        class="!tw-text-blue-700 hover:tw-underline"
+      >to read Jain Aagam</nuxt-link> of your choice.
+    </p>
+    <!-- <div v-if="content_book === null && content_part === null && content_chapter === null &&content_lesson === null && (content_sutra === undefined || content_sutra_original === undefined)"></div> -->
   </div>
 </template>
 
@@ -44,6 +22,4 @@ export default {
 </script>
 
 <style lang="sass" scoped>
-.list-item
-  @apply tw-inline-block tw-p-6 tw-border tw-border-gray-400 hover:tw-shadow-xl tw-text-center tw-text-gray-600
 </style>

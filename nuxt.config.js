acb102e1b64b860dc99630a4fd4e23740f4a1b81
CA Manas Madrecha <65993481+ManasMadrecha@users.noreply.github.com>
2021-07-02 01:30:33 +0530

    docs: rename content folder to "content-jainaagam"

--- nuxt.config.js
+++ nuxt.config.js
@@ -206,6 +206,7 @@ export default {
 
   // Content module configuration: https://go.nuxtjs.dev/config-content
   content: {
+    dir: 'content-jainaagam',
     tocDepth: 4,
     nestedProperties: ['body.children', 'aagam.title', 'order.cat', 'order.aagam', 'order.aagam.position', 'order.book', 'order.book.position', 'order.section', 'order.section.position', 'order.part', 'order.part.position', 'order.chapter', 'order.chapter.position', 'order.lesson', 'order.lesson.position', 'order.sutra', 'order.sutra.position', 'parent.type', 'children.type', 'children.count', 'children.children'],
     extendParser: {

4be468d93a8a67bccad23e384df8c81dacff6c7b
CA Manas Madrecha <65993481+ManasMadrecha@users.noreply.github.com>
2021-07-06 02:29:40 +0530

    feat(compo): Go top with vuetify

--- components/molecules/GoTop.vue
+++ components/molecules/GoTop.vue
@@ -1,43 +1,20 @@
 <template>
   <button
-    @click="GoUp()"
-    class="tw-fixed tw-z-40 tw-bottom-16 tw-right-2 tw-rounded-2xl tw-text-2xl tw-w-10 tw-h-10 lg:tw-w-12 lg:tw-h-12 tw-font-2xl tw-bg-gradient-to-bl tw-from-white tw-to-pink-200 tw-shadow-md focus:tw-outline-none focus:tw-ring focus:tw-ring-pink-300"
+    @click="$vuetify.goTo(0)"
+    v-ripple
+    class="tw-fixed tw-bottom-16 tw-right-1 tw-z-40 tw-rounded-2xl tw-text-2xl tw-w-10 tw-h-10 lg:tw-w-12 lg:tw-h-12 tw-font-2xl tw-bg-gradient-to-bl tw-from-white tw-to-pink-200 tw-shadow-md focus:tw-outline-none"
   >
-    👆🏻
-    <!-- <fa-icon icon="arrow-up" class="tw-text-pink-800"></fa-icon> -->
+    <v-icon class="!tw-text-pink-800"> {{mdiArrowUp}} </v-icon>
   </button>
 </template>
 
 <script>
-// import { library } from "@fortawesome/fontawesome-svg-core";
-// import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
-// library.add(faArrowUp);
-
+import { mdiArrowUp } from "@mdi/js";
 export default {
   data() {
     return {
-      // onTop: false,
+      mdiArrowUp
     };
-  },
-  mounted() {
-    // window.addEventListener("scroll", this.handleScroll);
-  },
-  destroyed() {
-    // window.removeEventListener("scroll", this.handleScroll);
-  },
-  methods: {
-    // handleScroll() {
-    //   this.onTop = window.pageYOffset > 200;
-    // },
-    GoUp() {
-      if (window.screen.width < 786) {
-        document.body.scrollTop = 5;
-        document.documentElement.scrollTop = 5;
-      } else {
-        document.body.scrollTop = 5;
-        document.documentElement.scrollTop = 5;
-      }
-    }
   }
 };
 </script>

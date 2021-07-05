5d8d414d336f82e4f396e9be0f355a3aba33b445
CA Manas Madrecha <65993481+ManasMadrecha@users.noreply.github.com>
2021-07-06 04:59:33 +0530

    style(compo): bg color in postChooseLang

--- components/organisms/post/PostChooseLang.vue
+++ components/organisms/post/PostChooseLang.vue
@@ -1,5 +1,5 @@
 <template>
-  <ol class="tw-flex tw-flex-wrap tw-justify-center tw-items-center tw-gap-4">
+  <ol class="tw-flex tw-flex-wrap tw-justify-center tw-items-center tw-gap-4 tw-bg-gradient-to-br tw-from-white tw-to-gray-50 tw-p-4">
     <li
       v-for="langPost in langPosts"
       :key="langPost.path"

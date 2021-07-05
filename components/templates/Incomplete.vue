f40769597d31c1b7612b9037d1cd0b9e42d8de30
CA Manas Madrecha <65993481+ManasMadrecha@users.noreply.github.com>
2021-07-06 03:27:21 +0530

    feat: new text in incomplete (but comment)

--- components/templates/Incomplete.vue
+++ components/templates/Incomplete.vue
@@ -1,12 +1,56 @@
 <template>
-  <!-- v-if="content_everyAagamFile === null || content_sutra_original === null" -->
-  <div class="tw-mt-3 tw-text-gray-600 tw-text-center">
-    <p class="">This page is yet to be added. </p>
-    <p>Go Home to <nuxt-link
-        :to="`/`"
-        class="!tw-text-blue-700 hover:tw-underline"
-      >read Jain Aagam</nuxt-link> of your choice.
+  <div class="tw-text-gray-600">
+    <p class="tw-my-3 tw-text-3xl">This page is yet to be added in English language.</p>
+    <p class="tw-my-3">But no worries, we will soon add this page.</p>
+    <p class="tw-my-3">Till then, you have these choices:
     </p>
-    <!-- <div v-if="content_book === null && content_part === null && content_chapter === null &&content_lesson === null && (content_sutra === undefined || content_sutra_original === undefined)"></div> -->
+    <ol class="tw-my-3 md:tw-my-6 tw-list-decimal tw-list-inside">
+      <li class="tw-my-3">
+        Just see below the title whether this page is available in alternate languages.
+      </li>
+      <li class="tw-my-3">
+        OR View <strong>Table of Contents</strong> or the <strong>Original sutra</strong> page by clicking on their respective links above.
+      </li>
+      <li class="tw-my-3">OR If this page has Contents of its own above, you can surely visit them.</li>
+      <li class="tw-my-3">
+        OR simply go to <nuxt-link
+          :to="`/`"
+          class="!tw-text-linkblue hover:tw-underline"
+        >Home Page</nuxt-link> to browse list of Jain Aagam
+      </li>
+    </ol>
+    <div class="tw-border-t tw-pt-2 tw-my-3 md:tw-my-6">
+
+      <p class="tw-my-3">Or better yet, why not add this page yourself in <b>a language of your choice</b> by clicking on Edit this page on Github link below.</p>
+      <p class="tw-my-3">Or just let us know that you want this page to be added by us ASAP by
+        <a
+          :href="`https://github.com/jainism-portal/jainaagam/issues/new?title=incomplete: ${$route.path}`"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="!tw-text-linkblue hover:tw-underline"
+        >creating an issue</a>
+        <v-icon small>{{mdiOpenInNew}}</v-icon> on our Github repository of <a
+          href="https://github.com/jainism-portal/jainaagam"
+          target="_blank"
+          rel="noopener"
+          title="Jain Aagam Github repository"
+          class="!tw-text-linkblue hover:tw-underline"
+        >
+          Jain Aagam
+        </a>
+        <v-icon small>{{mdiOpenInNew}}</v-icon>
+      </p>
+    </div>
   </div>
 </template>
+
+<script>
+import { mdiOpenInNew } from "@mdi/js";
+export default {
+  data() {
+    return {
+      mdiOpenInNew
+    };
+  }
+};
+</script>

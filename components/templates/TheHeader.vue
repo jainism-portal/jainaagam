6373e221920dad186e21bcb879f7642bdaafe7f1
CA Manas Madrecha <65993481+ManasMadrecha@users.noreply.github.com>
2021-07-08 06:56:03 +0530

    feat(header): aagam link; style; mm website; color
    - added menu links to acharanga and sutrakritanga, along with home
    
    - added my blog link in social icons
    
    - added colors to the social icons
    
    - fixed the height issue of navigation drawer by setting as min-h-[95vh]

--- components/templates/TheHeader.vue
+++ components/templates/TheHeader.vue
@@ -3,60 +3,71 @@
     <v-navigation-drawer
       app
       v-model="toggleMenu"
-      class="!tw-z-50"
+      class="!tw-z-50 !tw-min-h-[95vh] !tw-max-h-screen"
       clipped
     >
-      <div class="tw-h-full tw-flex tw-flex-col md:tw-flex-none">
-        <div class="md:tw-hidden tw-h-[10%] tw-mt-3 tw-pb-2 tw-border-b tw-border-gray-300 tw-flex tw-justify-center tw-items-center">
+      <div class="tw-h-full">
+
+        <div class="tw-py-3 lg:tw-hidden tw-border-b tw-border-gray-300 tw-flex tw-justify-center tw-items-center">
           <nuxt-link
             :to="'/'"
-            class="tw-inline-block tw-text-xl !tw-text-pink-600"
+            class="tw-text-2xl tw-text-pink-700 tw-border-none"
           >
             ꣽ Jain Aagam
           </nuxt-link>
         </div>
 
-        <div class="tw-h-[70%] md:tw-h-full tw-p-3 tw-text-gray-600 tw-overflow-y-auto">
-          <ol>
+        <div class="tw-h-[85%] tw-flex tw-flex-col tw-justify-between">
+
+          <ol class="!tw-pl-0 tw-mt-0">
             <li
               v-for="menu in menus"
               :key="menu.text"
+              class="tw-mx-1 tw-border-b tw-border-gray-200 hover:tw-border-pink-200"
             >
-              <nuxt-link :to="menu.link">
+              <nuxt-link
+                :to="menu.link"
+                class="!tw-block tw-text-gray-800 tw-border-none tw-my-1 tw-px-4 tw-py-2 hover:tw-translate-x-1 "
+              >
                 {{menu.text}}
-
               </nuxt-link>
             </li>
           </ol>
-        </div>
 
-        <footer class="tw-h-auto tw-p-2 tw-border-t tw-border-gray-300">
-          <a
-            href="https://github.com/jainism-portal/jainaagam"
-            target="_blank"
-            rel="noopener noreferrer"
-            title="Jain Aagam Github repository"
-            class="tw-inline-block tw-p-1"
-          >
-            <v-icon class="!tw-text-gray-600">{{mdiStar}}</v-icon> <span class="tw-text-linkblue hover:tw-underline"> Star us on Github</span>
-            <v-icon small>{{mdiOpenInNew}}</v-icon>
-          </a>
-          <p class="tw-p-1">&copy; CA Manas Madrecha</p>
-          <div class="tw-flex tw-flex-wrap">
+          <footer class="tw-border-t tw-border-gray-300 tw-pt-2 tw-text-center">
+            <v-icon>{{mdiStar}}</v-icon>
             <a
-              v-for="social in socials"
-              :key="social.link"
-              :href="social.link"
+              href="https://github.com/jainism-portal/jainaagam"
               target="_blank"
               rel="noopener noreferrer"
-              class="tw-inline-block tw-p-1 tw-rounded-xl hover:tw-shadow"
-              :class="`hover:tw-bg-${social.color}-50`"
-              :title="social.linkTitle"
-            >
-              <v-icon :class="`!tw-text-${social.color}-600`">{{social.icon}}</v-icon>
+              title="Jain Aagam Github repository"
+              class="tw-border-gray-100 hover:tw-border-linkpink"
+            >Star us on Github
+              <v-icon small>{{mdiOpenInNew}}</v-icon>
             </a>
-          </div>
-        </footer>
+            <div class="tw-mt-3">
+              <p class="!tw-my-0">&copy; CA Manas Madrecha</p>
+              <ul class="!tw-pl-0 tw-mt-1 tw-flex tw-flex-wrap tw-justify-center tw-space-x-2 tw-items-center">
+                <li
+                  v-for="social in socials"
+                  :key="social.link"
+                >
+                  <a
+                    :href="social.link"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="tw-border-none hover:tw-scale-125"
+                    :title="social.linkTitle"
+                  >
+                    <v-icon :class="`${social.color}`">{{social.icon}}</v-icon>
+                  </a>
+                  <span class="a11y-hide">{{social.linkTitle}}</span>
+                </li>
+              </ul>
+            </div>
+          </footer>
+
+        </div>
 
       </div>
     </v-navigation-drawer>
@@ -76,13 +87,13 @@
       </template>
       <v-icon
         @click.stop="toggleMenu = !toggleMenu"
-        class="!tw-text-gray-500 tw-mx-2 tw-transform hover:tw-scale-125 tw-transition-transform"
+        class="!tw-text-gray-500 tw-mx-2 tw-transform-gpu tw-transition-all hover:tw-scale-125 active:tw-scale-95 tw-scale-100"
       >{{mdiMenu}}</v-icon>
 
       <div class="tw-w-full tw-flex tw-justify-between tw-items-center">
         <nuxt-link
           :to="'/'"
-          class="tw-inline-block tw-mx-2 tw-p-2 !tw-text-pink-700 tw-font-semibold tw-text-xl md:tw-text-2xl tw-transform hover:tw--skew-x-6 tw-transition-transform"
+          class="tw-border-none tw-inline-block tw-mx-2 tw-p-2 !tw-text-pink-700 tw-font-semibold tw-text-2xl"
         >Jain Aagam</nuxt-link>
       </div>
     </v-app-bar>
@@ -93,6 +104,7 @@
 import {
   mdiOpenInNew,
   mdiMenu,
+  mdiWeb,
   mdiGithub,
   mdiInstagram,
   mdiLinkedin,
@@ -108,27 +120,36 @@ export default {
       toggleMenu: this.$vuetify.breakpoint.mdAndUp,
       toggleSearch: false,
       menus: [
+        { text: `🏠 Home`, link: `/` },
+        { text: `🙏🏻 Acharanga`, link: `/acharanga` },
+        { text: `🙏🏻 Sutrakritanga`, link: `/sutrakritanga` },
         // { text: `About`, link: `/about` },
-        { text: `Releases`, link: `/releases` }
+        { text: `📁 Releases`, link: `/releases` }
       ],
       socials: [
+        {
+          link: `https://manas.madrecha.com`,
+          linkTitle: `Manas Madrecha blog`,
+          icon: mdiWeb,
+          color: `!tw-text-gray-500`
+        },
         {
           link: `https://github.com/ManasMadrecha`,
           linkTitle: `Manas Madrecha Github`,
           icon: mdiGithub,
-          color: `gray`
+          color: `!tw-text-gray-700`
         },
         {
           link: `https://instagram.com/ManasMadrecha`,
           linkTitle: `Manas Madrecha Instagram follow`,
           icon: mdiInstagram,
-          color: `pink`
+          color: `!tw-text-instagram`
         },
         {
           link: `https://www.linkedin.com/in/manasmadrecha/`,
           linkTitle: `Manas Madrecha LinkedIn connect`,
           icon: mdiLinkedin,
-          color: `blue`
+          color: `!tw-text-linkedin`
         }
       ]
     };

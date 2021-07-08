e38a0303b7e5c11f76b1572a3cb1ca21ddb35cbb
CA Manas Madrecha <65993481+ManasMadrecha@users.noreply.github.com>
2021-07-08 06:53:19 +0530

    feat(SutraOriginal): tabs replaced with details
    - tabs offer single view;
    with flexed details I can see og + transliteration at the same time
    
    - no need to toggle the h2 now
    
    - added h3 as original and transliteration inside the summary tag
    
    - no need to import all.sass file or override v-tabs classes

--- components/templates/post/SutraOriginal.vue
+++ components/templates/post/SutraOriginal.vue
@@ -1,57 +1,37 @@
 <template>
   <section>
-    <header
-      @click="toggleOriginal = !toggleOriginal"
-      class="tw-group tw-cursor-pointer tw-my-3 md:tw-my-6 tw-text-pink-800 tw-text-xl md:tw-text-3xl tw-bg-gradient-to-r tw-from-white tw-to-red-50 tw-rounded-md tw-py-1 tw-flex tw-flex-wrap tw-justify-between tw-items-center"
-    >
-      <h2>Jain Aagam Sutra</h2>
-      <v-icon class="tw-mx-4 !tw-text-pink-800">{{!toggleOriginal ? mdiChevronDown : mdiChevronUp}}</v-icon>
-    </header>
-    <v-tabs
-      show-arrows
-      :slider-size="1"
-      v-show="toggleOriginal"
-    >
-      <v-tab
-        data-tab
-        active-class="tw-bg-gradient-to-t tw-from-white tw-via-white tw-to-yellow-50 !tw-text-yellow-900"
-      >
-        <span class="tw-capitalize">Original</span>
-      </v-tab>
-      <v-tabs-slider class="tw-bg-gradient-to-br tw-from-white tw-to-yellow-400"></v-tabs-slider>
-      <v-tab-item
-        lang="pka-Deva"
-        class="tw-mt-3 tw-text-gray-600 tw-overflow-y-auto tw-max-h-[40vh]"
-        eager
-      >
-        <nuxt-content
-          :document="sutraOriginal"
-          class="tw-border tw-px-2 md:tw-px-4"
-        ></nuxt-content>
-      </v-tab-item>
-      <v-tab
-        data-tab
-        active-class="tw-bg-gradient-to-t tw-from-white tw-via-white tw-to-yellow-50 !tw-text-yellow-900"
-      >
-        <span class="tw-capitalize">Transliteration</span>
-      </v-tab>
-      <v-tab-item
-        lang="pkr-Latn"
-        class="tw-mt-6 tw-text-gray-600 tw-overflow-y-auto tw-max-h-[40vh]"
-        eager
-      >
-        <nuxt-content
-          :document="sutraLatn"
-          class="tw-border tw-px-2 md:tw-px-4"
-        ></nuxt-content>
-      </v-tab-item>
-    </v-tabs>
+    <h2 class="nuxt-content-h2">Aagam Sutra</h2>
+    <div class="tw-my-3 md:tw-my-6 tw-flex tw-flex-col md:tw-flex-row tw-flex-wrap tw-justify-around tw-items-stretch tw-gap-2">
+      <div class="md:tw-w-[calc(50%-0.5rem)]">
+        <details class="tw-w-full tw-my-0">
+          <summary>
+            <h3 class="tw-my-0 tw-inline">Original</h3>
+          </summary>
+          <nuxt-content
+            :document="sutraOriginal"
+            lang="pka-Deva"
+          ></nuxt-content>
+        </details>
+      </div>
+      <div class="md:tw-w-[calc(50%-0.5rem)]">
+        <details class="tw-w-full tw-my-0">
+          <summary>
+            <h3 class="tw-my-0 tw-inline">
+              Transliteration
+            </h3>
+          </summary>
+          <nuxt-content
+            :document="sutraLatn"
+            lang="pka-Latn"
+          ></nuxt-content>
+        </details>
+      </div>
+    </div>
     <nuxt-link
       :to="`/${AagamName}/original#${sutraOriginal.depth}`"
-      class="tw-text-base tw-underline !tw-text-gray-500 "
-    >Read full {{AagamName}} original sutra</nuxt-link>
+      class="tw-inline tw-text-base tw-border-gray-300"
+    >Read full {{AagamName}} original sutra here</nuxt-link>
   </section>
-
 </template>
 
 <script>
@@ -102,13 +82,3 @@ export default {
   }
 };
 </script>
-
-<style lang="sass" scoped>
-.v-slide-group__next, .v-slide-group__prev
-  flex: 0 1 32px !important
-// [data-tab]
-//   @apply tw-text-sm md:tw-text-base
-</style>
-
-<style lang="sass" src="~/assets/css/all.sass" scoped>
-</style>
